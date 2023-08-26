095385731e4583322e61769d9c760b2f81f639e3
Ritik <ritikpatel705@gmail.com>
2023-08-26 23:34:47 +0530

    reset password live link update

--- controllers/authController.js
+++ controllers/authController.js
@@ -195,8 +195,8 @@ module.exports.forgotPassword = async (req, res) => {
     let token = jwt.sign({ userId: user._id }, secret, {
       expiresIn: "15m",
     });
-    // const link = `https://gauravnodejsauthentication.onrender.com/resetPassword/${user._id}/${token}`;
-    const link = `http://localhost:5000/resetPassword/${user._id}/${token}`;
+    const link = `https://nodejs-authentication-system-hf37.onrender.com/resetPassword/${user._id}/${token}`;
+    // const link = `http://localhost:5000/resetPassword/${user._id}/${token}`;
     
     //...................................Nodemailer code.......................................//
     const transporter = nodemailer.createTransport({
